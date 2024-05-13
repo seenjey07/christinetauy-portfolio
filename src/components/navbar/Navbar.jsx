cbc688f64c3645e40fad8c77f55af67c36c2a3d9
Christine Rosales <138593141+seenjey07@users.noreply.github.com>
2024-05-13 18:02:44 +0800

    Added animation to the span tag.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,18 @@
 import "./navbar.scss";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       {/* <Sidebar /> */}
       <div className="wrapper">
-        <span>Christine Tauy</span>
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 2 }}
+        >
+          Christine Tauy
+        </motion.span>
         <div className="social">
           <a href="https://www.linkedin.com/in/christinerosalesprofile/">
             <img src="/linkedin.png" alt="linkedin"></img>
