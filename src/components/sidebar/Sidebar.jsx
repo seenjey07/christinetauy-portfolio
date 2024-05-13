63103b655bf184589a9b232181da4b8892abf0b7
Christine Rosales <138593141+seenjey07@users.noreply.github.com>
2024-05-13 20:55:44 +0800

    Moved the variants before the component declaration.

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,31 +1,31 @@
 import { useState } from "react";
 import "./sidebar.scss";
 import Links from "./links/Links";
-import ToggleButton from "./toggleButton/ToggleButton";
+import ToggleButton from "./toggleButton/toggleButton";
 import { motion } from "framer-motion";
+
+const variants = {
+  open: {
+    clipPath: "circle(1200px at 80px 80px)",
+    transition: {
+      type: "spring",
+      stiffness: 60,
+      damping: 30,
+    },
+  },
+  closed: {
+    clipPath: "circle(30px at 50px 50px)",
+    transition: {
+      delay: 0.2,
+      type: "spring",
+      stiffness: 400,
+      damping: 20,
+    },
+  },
+};
 
 const Sidebar = () => {
   const [open, setOpen] = useState(false);
-
-  const variants = {
-    open: {
-      clipPath: "circle(1200px at 80px 80px)",
-      transition: {
-        type: "spring",
-        stiffness: 60,
-        damping: 30,
-      },
-    },
-    closed: {
-      clipPath: "circle(30px at 50px 50px)",
-      transition: {
-        delay: 0.2,
-        type: "spring",
-        stiffness: 400,
-        damping: 20,
-      },
-    },
-  };
 
   return (
     <motion.div className="sidebar" animate={open ? "open" : "closed"}>
