364b2f22d05677443b2063ceb94c75496308a09b
Christine Rosales <138593141+seenjey07@users.noreply.github.com>
2024-05-14 19:17:30 +0800

    Added text, buttons, and scroll icon.

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,6 +3,15 @@ import "./home.scss";
 const Home = () => {
   return (
     <div className="home">
+      <div className="textContainer">
+        <h2>Christine Tauy</h2>
+        <h1>Web Developer</h1>
+        <div className="buttons">
+          <button>Projects</button>
+          <button>Contact Me</button>
+        </div>
+        <img src="/scroll.png" alt="Scroll Icon" />
+      </div>
       <div className="imageContainer">
         <img src="/Christine.jpg" alt="Christine"></img>
       </div>
