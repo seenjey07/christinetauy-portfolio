9a9d95642821fb3b6c5f59b3eb98d20e9a84c0ca
Christine Rosales <138593141+seenjey07@users.noreply.github.com>
2024-05-14 19:36:02 +0800

    Added styles to the texts, buttons, and icon.

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,17 +3,19 @@ import "./home.scss";
 const Home = () => {
   return (
     <div className="home">
-      <div className="textContainer">
-        <h2>Christine Tauy</h2>
-        <h1>Web Developer</h1>
-        <div className="buttons">
-          <button>Projects</button>
-          <button>Contact Me</button>
+      <div className="wrapper">
+        <div className="textContainer">
+          <h2>CHRISTINE TAUY</h2>
+          <h1>Web Developer</h1>
+          <div className="buttons">
+            <button>My Projects</button>
+            <button>Contact Me</button>
+          </div>
+          <img className="scroll" src="/scroll.png" alt="Scroll Icon" />
         </div>
-        <img src="/scroll.png" alt="Scroll Icon" />
       </div>
       <div className="imageContainer">
-        <img src="/Christine.jpg" alt="Christine"></img>
+        <img className="myPhoto" src="/Christine.jpg" alt="Christine"></img>
       </div>
     </div>
   );
