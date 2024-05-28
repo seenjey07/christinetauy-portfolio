28116479364f2d4f2403b012e87ef3297136c8ec
Christine Rosales <138593141+seenjey07@users.noreply.github.com>
2024-05-28 19:50:32 +0800

    Created an item array containing my projects details.

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,7 +1,29 @@
 import "./projects.scss";
 
+const items = [
+  {
+    id: 1,
+    title: "Tic-Tac-Toe",
+    description:
+      "While learning about Javascript, Grids, and Flex, I created this site as one of our projects: an interactive Tic-Tac-Toe game that brings the classic 2-player challenge to life. I used CSS for styling.",
+    img: "https://i.ibb.co/w7kVLgk/image.png",
+  },
+  {
+    id: 2,
+    title: "Baking App ~ Prosperity Bank",
+    description:
+      "This was one of my first projects while learning React. It is a banking application that enables users to create accounts and manage their finances. It utilizes CSS for a polished visual presentation. The app provides users with an intuitive dashboard featuring tabs for financial transactions.",
+    img: "https://i.ibb.co/TLrMZtr/image.png",
+  },
+];
+
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div className="projects">
+      <h1>Projects</h1>
+      <h2>Coming </h2>
+    </div>
+  );
 };
 
 export default Projects;
