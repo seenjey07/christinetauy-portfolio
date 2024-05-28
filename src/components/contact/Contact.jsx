1022a1d1ca0b1ceaee3662d40c29f3a2ec56a717
Christine Rosales <138593141+seenjey07@users.noreply.github.com>
2024-05-29 06:17:08 +0800

    Initialized the contact folder and files.

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,11 @@
+import "./contact.scss";
+
+const Contact = () => {
+  return (
+    <div className="contact">
+      <h1 className="container">Let&apos;s work together</h1>
+    </div>
+  );
+};
+
+export default Contact;
