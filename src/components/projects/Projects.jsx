7bb5d3a18ef9c333aee712e0a3a8fb987c188c02
Christine Rosales <138593141+seenjey07@users.noreply.github.com>
2024-05-28 21:16:17 +0800

    Added sample works in the items array.

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -15,13 +15,34 @@ const items = [
       "This was one of my first projects while learning React. It is a banking application that enables users to create accounts and manage their finances. It utilizes CSS for a polished visual presentation. The app provides users with an intuitive dashboard featuring tabs for financial transactions.",
     img: "https://i.ibb.co/TLrMZtr/image.png",
   },
+  {
+    id: 3,
+    title: "Weather App ~ WeatherPulse",
+    description:
+      "A sleek and responsive weather application built with React and styled using Tailwind CSS. It fetches real-time weather data from an API and displays the current temperature, weather conditions, and a 5-day forecast for any searched location. Designed for ease of use, it offers a clean and intuitive user interface for checking the weather quickly and efficiently.",
+    img: "https://img.freepik.com/free-vector/collection-cartoon-weather-signs_79603-1806.jpg?t=st=1716902003~exp=1716905603~hmac=3ac5019995f07fa7418d10103e9d8ff2ff77f294cf310dd29633774143ff7801&w=826",
+  },
 ];
+
+const EachProject = ({ item }) => {
+  return (
+    <section>
+      {item.title}
+      <img src={item.img} alt={item.title} />
+    </section>
+  );
+};
 
 const Projects = () => {
   return (
     <div className="projects">
-      <h1>Projects</h1>
-      <h2>Coming </h2>
+      <div className="progress">
+        <h1>Featured Works</h1>
+        <div className="progressBar"></div>
+      </div>
+      {items.map((item) => (
+        <EachProject key={item.id} item={item} />
+      ))}
     </div>
   );
 };
