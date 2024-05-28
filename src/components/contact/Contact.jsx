2e3136a854049aaec5783b97513d732796f1d313
Christine Rosales <138593141+seenjey07@users.noreply.github.com>
2024-05-29 06:48:42 +0800

    Added textContainer with h1, and contactDetails with h2 and span tags.

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,25 @@ import "./contact.scss";
 const Contact = () => {
   return (
     <div className="contact">
-      <h1 className="container">Let&apos;s work together</h1>
+      <div className="textContainer">
+        <h1>Let&apos;s work together!</h1>
+        <div className="contactDetails">
+          <h2>Mail 📧</h2>
+          <span>
+            <a href="mailto:chrosales07@gmail.com">chrosales07@gmail.com</a>
+          </span>
+        </div>
+        <div className="contactDetails">
+          <h2>Address 🏠</h2>
+          <span>Tanza, Cavite, Philippines</span>
+        </div>
+        <div className="contactDetails">
+          <h2>Phone 📞</h2>
+          <span>+63950-303-3001</span>
+        </div>
+      </div>
+
+      <div className="formContainer"></div>
     </div>
   );
 };
