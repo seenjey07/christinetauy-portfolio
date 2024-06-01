92dd1036e9c87a82970a8429b668ea1eb06f0871
Christine Rosales <138593141+seenjey07@users.noreply.github.com>
2024-06-01 11:32:04 +0800

    Created the cursor file using useState, useEffect, and motion from framer. Also created the style file.

---
+++ src/components/cursor/Cursor.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import "./cursor.scss";
+import { motion } from "framer-motion";
+
+const Cursor = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      setPosition({ x: e.clientX, y: e.clientY });
+    };
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  return (
+    <motion.div
+      className="cursor"
+      animate={{ x: position.x + 10, y: position.y + 10 }}
+    ></motion.div>
+  );
+};
+
+export default Cursor;
