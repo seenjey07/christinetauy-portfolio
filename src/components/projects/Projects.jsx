d35b70e9c799d1b50b1d768318417db1b0550806
Christine Rosales <chrosales07@gmail.com>
2025-08-23 10:06:13 +0800

    Updated project list

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -5,14 +5,6 @@ import { useRef } from "react";
 const items = [
   {
     id: 1,
-    title: "Tic-Tac-Toe",
-    description:
-      "While learning about Javascript, Grids, and Flex, I created this site as one of our projects: an interactive Tic-Tac-Toe game that brings the classic 2-player challenge to life. I used CSS for styling.",
-    img: "https://i.ibb.co/MsHyTfW/Tic-Tac-Toe.png",
-    link: "https://seenj-tictactoe.netlify.app/",
-  },
-  {
-    id: 2,
     title: "Prosperity Bank",
     description:
       "This was one of my first projects while learning React. It is a banking application that enables users to create accounts and manage their finances. It utilizes CSS for a polished visual presentation. The app provides users with an intuitive dashboard featuring tabs for financial transactions.",
@@ -20,7 +12,7 @@ const items = [
     link: "https://seenj-prosperity-bank.netlify.app/",
   },
   {
-    id: 3,
+    id: 2,
     title: "NimbusAirways",
     description:
       "NimbusAirways is a full-stack airline booking app developed as my final project for Avion School. Its backend is built with Ruby on Rails, while the frontend uses ReactJS compiled with Vite. Tailwind CSS, DaisyUI, and ShadCN were used to achieve a clean and modern design. The app is deployed with Render for the backend and Vercel for the frontend.",
@@ -28,7 +20,7 @@ const items = [
     link: "https://nimbusairways.vercel.app/",
   },
   {
-    id: 4,
+    id: 3,
     title: "WeatherPulse",
     description:
       "A sleek and responsive weather application built with React and styled using Tailwind CSS. It fetches real-time weather data from OpenWeather API. It offers a clean and intuitive user interface for checking the weather quickly and efficiently.",
@@ -36,7 +28,7 @@ const items = [
     link: "https://seenj-weatherpulse.netlify.app",
   },
   {
-    id: 5,
+    id: 4,
     title: "ChoreChatter",
     description:
       "ChoreChatter is a simple and interactive chat and to-do application designed to help users manage their daily tasks in a fun and engaging way by combining a mock chatbot feature with a to-do list.",
@@ -44,7 +36,7 @@ const items = [
     link: "https://seenj-chorechatter.netlify.app/",
   },
   {
-    id: 6,
+    id: 5,
     title: "Professional Review Buddy Website",
     description:
       "Using Figma, I created a design for a website for a professional review company. This website is currently under construction.",
@@ -52,7 +44,7 @@ const items = [
     link: "https://bit.ly/PRBHomeFigmaProto",
   },
   {
-    id: 7,
+    id: 6,
     title: "Professional Review Buddy - PRB LET Website",
     description:
       "Using Figma, I created a design for Professional Review Buddy's related website - PRB LET, specifically for Filipino LET takers. This website is currently under construction.",
@@ -60,20 +52,12 @@ const items = [
     link: "https://bit.ly/PRBLET-Page",
   },
   {
-    id: 8,
+    id: 7,
     title: "MindfulMe",
     description:
       "Currently under development, MindfulMe is a wellness app that seeks to support users in nurturing their mental health and embracing mindfulness daily. It allows users to record daily moods and journal entries, fostering self-awareness and emotional reflection. Built with React, Vite, JSX, Tailwind CSS, and Ruby on Rails, the app aims to deliver a modern, intuitive user experience.",
     img: "https://i.ibb.co/LkgtHHx/Mindful-Me.png",
     link: "",
-  },
-  {
-    id: 9,
-    title: "ShopBud",
-    description:
-      "With ShopBud, users can easily manage their shopping lists, including adding new items, editing existing entries, and deleting items they no longer need. Stay organized and make your shopping experience smoother with ShopBud.",
-    img: "https://i.ibb.co/6y3R3m7/ShopBud.png",
-    link: "https://seenj-shopbud.netlify.app/",
   },
 ];
 
